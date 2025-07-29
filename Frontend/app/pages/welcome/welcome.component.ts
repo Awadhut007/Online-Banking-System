0887caf57401547d96a3d6614fc7e08510e8ae41
Awadhut007 <128478109+Awadhut007@users.noreply.github.com>
2025-07-29 15:25:47 +0530

    Add files via upload

---
+++ Frontend/app/pages/welcome/welcome.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css'],
+  imports: [RouterModule, NavbarComponent] // ✅ Add RouterModule here
+})
+export class WelcomeComponent {}
